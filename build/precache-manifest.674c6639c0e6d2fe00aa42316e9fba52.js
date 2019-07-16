8d818cf27687207914822014ccd1a40aa269db39
badmofo <shubham.sy570@gmail.com>
2019-07-16 23:58:15 +0530

    push

---
+++ build/precache-manifest.674c6639c0e6d2fe00aa42316e9fba52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3e16d0e5ddfa2b089ffe433a378ca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ebf0b25d4d0c7d88f3f",
+    "url": "/static/css/main.6044d4eb.chunk.css"
+  },
+  {
+    "revision": "0a71b94b727b9ac79e80",
+    "url": "/static/js/2.39be5d9e.chunk.js"
+  },
+  {
+    "revision": "1ebf0b25d4d0c7d88f3f",
+    "url": "/static/js/main.dad8e289.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
